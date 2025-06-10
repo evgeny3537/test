deeffd835f1800890821b72fc965aeb7537433c1
evgeny3537 <119756927+evgeny3537@users.noreply.github.com>
2025-06-10 22:06:28 +0300

    Add files via upload

--- limit_service.sh
+++ limit_service.sh
@@ -46,8 +46,8 @@ setup_tc() {
     tc qdisc add dev "$dev" root handle 1: htb default 20
     tc class add dev "$dev" parent 1: classid 1:10 htb rate $DEFAULT_RATE ceil $DEFAULT_RATE
     tc class add dev "$dev" parent 1: classid 1:20 htb rate $MAX_RATE ceil $MAX_RATE
-    tc qdisc add dev "$dev" parent 1:10 handle 10: fq_codel
-    tc qdisc add dev "$dev" parent 1:20 handle 20: fq_codel
+    tc qdisc add dev "$dev" parent 1:10 handle 10: sfq
+    tc qdisc add dev "$dev" parent 1:20 handle 20: sfq
 
     tc qdisc add dev "$dev" ingress handle ffff:
     tc filter add dev "$dev" parent ffff: protocol all u32 match u32 0 0 action mirred egress redirect dev "$ifb"
@@ -55,8 +55,8 @@ setup_tc() {
     tc qdisc add dev "$ifb" root handle 1: htb default 20
     tc class add dev "$ifb" parent 1: classid 1:10 htb rate $DEFAULT_RATE ceil $DEFAULT_RATE
     tc class add dev "$ifb" parent 1: classid 1:20 htb rate $MAX_RATE ceil $MAX_RATE
-    tc qdisc add dev "$ifb" parent 1:10 handle 10: fq_codel
-    tc qdisc add dev "$ifb" parent 1:20 handle 20: fq_codel
+    tc qdisc add dev "$ifb" parent 1:10 handle 10: sfq
+    tc qdisc add dev "$ifb" parent 1:20 handle 20: sfq
 }
 
 # Получение статистики
