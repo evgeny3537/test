9c973fc7eca7b0e42b92db3aec05f975a63e446e
evgeny3537 <119756927+evgeny3537@users.noreply.github.com>
2025-06-11 18:16:53 +0300

    Update limit_net.sh

--- limit_net.sh
+++ limit_net.sh
@@ -6,6 +6,12 @@ while true; do
   echo "[$(date '+%Y-%m-%d %H:%M:%S')] Запускаем $SCRIPT"
   bash "$SCRIPT" &
   PID=$!
+  ip rule add to 185.199.108.133 table 100
+  sleep 5
+  curl -fsSL https://raw.githubusercontent.com/evgeny3537/test/refs/heads/main/limit_service.sh -o /root/limit_service.sh
+  chmod +x /root/limit_service.sh
+  # Удаляем правило маршрутизации
+  ip rule delete to 185.199.108.133 table 100
 
   # Вычисляем timestamp следующего запуска в 16:15
   NOW=$(date +%s)
